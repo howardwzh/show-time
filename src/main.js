14ddda4b22dc3b6433c61ff5e404c48cf8c70727
王忠华 <wangzhonghua@ppmoney.com>
2018-06-01 14:17:24 +0800

    feat: 引入vue-router

--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.js'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import NotFound from './NotFound.vue'
 import HTML5 from './pages/html5/Index.vue'
 import CSS3 from './pages/css3/Index.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter) 
 
-const routes = {
-  '/': App,
-  '/html5': HTML5,
-  '/css3': CSS3
-}
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: App },
+    { path: '/html5', component: HTML5 },
+    { path: '/css3', component: CSS3 },
+    { path: '*', component: NotFound }
+  ]
+})
 
 new Vue({
   el: '#app',
-  data: {
-    currentRoute: window.location.pathname
-  },
-  computed: {
-    ViewComponent () {
-      return routes[this.currentRoute] || NotFound
-    }
-  },
-  render (h) { return h(this.ViewComponent)}
+  router,
+  template: '<router-view></router-view>'
 })
