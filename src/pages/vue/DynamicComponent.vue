3478e9b3d6503744087c73b6fadb9bff3c3387b0
王忠华 <wangzhonghua@ppmoney.com>
2018-06-29 16:54:05 +0800

    update DynamicComponent

--- src/pages/vue/DynamicComponent.vue
+++ src/pages/vue/DynamicComponent.vue
@@ -1,5 +1,44 @@
 <template>
   <Group title="">
+    <WzhMarkdown>
+      <textarea>
+##### 代码如下:
+```js
+import ComponentA from './ComponentA'
+import ComponentB from './ComponentB'
+
+export default {
+  name: 'DynamicComponent',
+  data () {
+    return {
+      component: ComponentA
+    }
+  },
+  methods: {
+    changeComponent (key) {
+      if (key === 'A') {
+        this.component = ComponentA
+      } else {
+        this.component = ComponentB
+      }
+    }
+  }
+}
+```
+##### html代码:
+```html
+<p>
+  <button @click="changeComponent('A')">Posts</button><button @click="changeComponent('B')">Archive</button>
+</p>
+<h4>切换会刷新</h4>
+<component :is="component"></component>
+<h4>切换不会刷新</h4>
+<keep-alive>
+  <component :is="component"></component>
+</keep-alive>
+```
+      </textarea>
+    </WzhMarkdown>
     <p><button @click="changeComponent('A')">Posts</button><button @click="changeComponent('B')">Archive</button></p>
     <h4>切换会刷新</h4>
     <component :is="component"></component>
